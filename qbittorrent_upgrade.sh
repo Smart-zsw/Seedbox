4ec1b060f44363f1ee9f888b1cd15e3b26323118
Smart-zsw <98108711+Smart-zsw@users.noreply.github.com>
2025-11-26 22:14:12 +0800

    替换使用服务器地址

--- qbittorrent_upgrade.sh
+++ qbittorrent_upgrade.sh
@@ -8,10 +8,10 @@ echo "检测到系统架构: $ARCH"
 
 # 根据架构选择下载链接
 if [[ "$ARCH" == "x86_64" || "$ARCH" == "amd64" ]]; then
-    DOWNLOAD_URL="https://alist.zswnet.com:8443/d/OneDrive/Per/%E9%99%84%E4%BB%B6/qbittorrent-nox_x64-504"
+    DOWNLOAD_URL="https://alist.zswclub.com/d/qb_upgrade/qbittorrent-nox_x64-504"
     echo "使用 x86_64 架构版本"
 elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
-    DOWNLOAD_URL="https://alist.zswnet.com:8443/d/OneDrive/Per/%E9%99%84%E4%BB%B6/qbittorrent-nox_arm64_504"
+    DOWNLOAD_URL="https://alist.zswclub.com/d/qb_upgrade/qbittorrent-nox_arm64_504"
     echo "使用 ARM64 架构版本"
 else
     echo "错误:不支持的架构 $ARCH"
